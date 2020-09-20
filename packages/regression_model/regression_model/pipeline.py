5d9efeb16f9e733c6992605d764473cd1650d3f2
adityavyasbme <adityavyas1603@gmail.com>
2020-09-20 10:22:48 -0700

    Created Structure of the Project

---
+++ packages/regression_model/regression_model/pipeline.py
@@ -0,0 +1,18 @@
+
+from sklearn.pipeline import Pipeline
+
+import preprocessors as pp
+
+# categorical variables to encode
+CATEGORICAL_VARS = ['MSZoning', 'Neighborhood', 'RoofStyle', 'MasVnrType',
+                    'BsmtQual', 'BsmtExposure', 'HeatingQC', 'CentralAir',
+                    'KitchenQual', 'FireplaceQu', 'GarageType', 'GarageFinish',
+                    'PavedDrive']
+
+
+price_pipe = Pipeline(
+    [
+        ('categorical_encoder',
+            pp.CategoricalEncoder(variables=config.CATEGORICAL_VARS))
+    ]
+)
