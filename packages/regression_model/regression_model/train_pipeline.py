6e963a5bbca4dd7a42b7f4037898e586fc2a60f4
adityavyasbme <adityavyas1603@gmail.com>
2020-09-20 12:42:36 -0700

    Tested OOP structured Regression model

--- packages/regression_model/regression_model/train_pipeline.py
+++ packages/regression_model/regression_model/train_pipeline.py
@@ -1,14 +1,10 @@
-import pathlib
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import joblib
 
-PACKAGE_ROOT = pathlib.Path(__file__).resolve().parent
-TRAINED_MODEL_DIR = PACKAGE_ROOT/'trained_models'
-DATASET_DIR = PACKAGE_ROOT/'datasets'
-
-
-
-TESTING_DATA_FILE = DATASET_DIR/ 'test.csv'
-TRAINING_DATA_FILE = DATASET_DIR/ 'test.csv'
-TARGET = 'SalePrice'
+from regression_model import pipeline
+from regression_model.config import config
 
 # input variables 
 FEATURES = ['MSSubClass', 'MSZoning', 'Neighborhood',
@@ -21,12 +17,32 @@ FEATURES = ['MSSubClass', 'MSZoning', 'Neighborhood',
             # this one is only to calculate temporal variable:
             'YrSold']
 
-def save_pipeline()->None:
-    pass
+def save_pipeline(*,pipeline_to_persist)->None:
+    """Persist the pipeline"""
+    
+    save_file_name="regression_model.pkl"
+    save_path = config.TRAINED_MODEL_DIR/save_file_name
+    joblib.dump(pipeline_to_persist,save_path)
+    print("Saved pipeline")
 
 def run_training():
     """Train the model."""
-    print("Training...")
+    # read training data
+    data = pd.read_csv(config.DATASET_DIR / config.TRAINING_DATA_FILE)
+    
+    # divide train and test
+    
+    X_train, X_test, y_train, y_test = train_test_split(
+        data[config.FEATURES], data[config.TARGET], test_size=0.1, random_state=0
+    )  # we are setting the seed here
+
+    # transform the target
+    y_train = np.log(y_train)
+
+    pipeline.price_pipe.fit(X_train[config.FEATURES], y_train)
+
+    save_pipeline(pipeline_to_persist=pipeline.price_pipe)
+
 
 
 if __name__ == '__main__':
