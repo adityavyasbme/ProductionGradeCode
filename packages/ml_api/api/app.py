da488f61afc0579f79e67ec65b072fd9d3bff5d9
adityavyasbme <adityavyas1603@gmail.com>
2020-09-20 18:15:40 -0700

    Added logging and testing of api

--- packages/ml_api/api/app.py
+++ packages/ml_api/api/app.py
@@ -1,12 +1,20 @@
 from flask import Flask
 
-def create_app() -> Flask:
-	"""Create a flask app instance."""
+from api.config import get_logger
 
-	flask_app = Flask('ml_api')
 
-	#import blueprints
-	from api.controller import prediction_app
-	flask_app.register_blueprint(prediction_app)
+_logger = get_logger(logger_name=__name__)
 
-	return flask_app
+
+def create_app(*, config_object) -> Flask:
+    """Create a flask app instance."""
+
+    flask_app = Flask('ml_api')
+    flask_app.config.from_object(config_object)
+
+    # import blueprints
+    from api.controller import prediction_app
+    flask_app.register_blueprint(prediction_app)
+    _logger.debug('Application instance created')
+
+    return flask_app
